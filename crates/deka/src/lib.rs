9cd1899503dda37e6a66dbd68024a49b06bef6ef
luxluth <delphin.blehoussi93@gmail.com>
2025-11-26 17:30:53 +0100

    updating vulkan example

--- crates/deka/src/lib.rs
+++ crates/deka/src/lib.rs
@@ -250,6 +250,7 @@ impl DAL {
             width: size!(fit),
             height: size!(fit),
             padding: heka::sizing::Padding::new_lr_tb(4, 2),
+            margin: heka::sizing::Margin::new_lr_tb(0, 4),
             background_color: heka::color::Color::new(200, 200, 200, 255),
             layout: layout!(flex),
         });
